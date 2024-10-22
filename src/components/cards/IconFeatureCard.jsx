9da3d6f5e596840b3cfc5fd96f4adece79607e49
Edison Matos <edison.matos@live.com>
2024-10-22 15:12:56 -0300

    feat: Correções Rinaldo

--- src/components/cards/IconFeatureCard.jsx
+++ src/components/cards/IconFeatureCard.jsx
@@ -1,9 +1,9 @@
 export default function IconFeatureCard(props) {
-  const { icon, title, paragraph, className, children  } = props;
+  const { icon, title, paragraph, className, children } = props;
 
   return (
     <div
-      className={`w-full tablet1:h-[340px] p-0 tablet1:w-[290px] tablet2:h-[350px] desktop1:w-[260px] desktop1:h-[350px] desktop2:h-[360px] flex flex-col items-center desktop1:hover:scale-110 transition desktop1:p-0 ${className}`}
+      className={`w-full tablet1:h-[340px] p-0 tablet1:w-[290px] tablet2:h-[350px] desktop1:w-[260px] desktop1:h-[330px] desktop2:h-[330px] flex flex-col items-center desktop1:hover:scale-110 transition desktop1:p-0 ${className}`}
     >
       <div className="h-[64px] w-[64px] mb-[24px] bg-primary rounded-md flex justify-center items-center text-secondary">
         {icon}
@@ -14,7 +14,7 @@ export default function IconFeatureCard(props) {
       <p className="text-center text-black opacity-70 font-mainFont w-[90%] pb-4">
         {paragraph}
       </p>
-      {children }
+      {children}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#D0B160", // Com primária (variação clara)
+        primary: "#DBB14C", // Com primária (variação clara)
         secondary: "#191919", // Cor primária (variação escura)
         bgSectionLight: "#FFFFFF",
         bgSectionDark: "#3A6351",
